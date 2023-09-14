9343ab7fc53826d2fdf9d0617b337b704000bc89
alexey-zhukov <alexey.zhukov@gmail.com>
2023-09-14 18:00:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-9x2yan
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-9x2yan)
---
+++ app/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <Page>
+    <GridLayout>
+      <Button>Test</Button>
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+
+export default Vue.extend({
+  computed: {
+    message() {
+      return 'Blank {N}-Vue app!';
+    },
+  },
+
+  methods: {
+    logMessage() {
+      console.log('You have tapped the message!23');
+    },
+  },
+});
+</script>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
